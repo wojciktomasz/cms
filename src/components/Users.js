19158f11d0fdc76316802a01213fd1fb429f8fd7
Tomasz <wojciktomasz@o2.pl>
2017-09-20 11:01:28 +0200

    User data table displaying sorted users from the json file fully working

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import users from '../data/users.json'
+import { Table, Grid } from 'semantic-ui-react'
 
 
 class Users extends React.Component {
+
   render() {
     return (
       <div>
-        <h1>Users</h1>
-        <h1>Users</h1>
-        <h1>Users</h1>
-        <h1>Users</h1>
+        <Grid
+          textAlign='center'
+          style={{height: '100%'}}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{maxWidth: 850}}>
+            <Table className="ui celled padded table" style={{marginTop: '10%'}}>
+              <thead>
+              <tr>
+                <th className="single line ui center aligned">Last name</th>
+                <th className="single line ui center aligned">First name</th>
+                <th className="single line ui center aligned">e-mail</th>
+                <th className="single line ui center aligned">type</th>
+              </tr>
+              </thead>
+              <tbody>
+                {
+                  users
+                    .sort((a, b) => a.email.localeCompare(b.email))
+                    .map(
+                    user => (
+                      <tr key={user.email} className="ui center aligned">
+                        <td>{user.LastName}</td>
+                        <td>{user.name}</td>
+                      <td>{user.email}</td>
+                        <td>{user.type}</td>
+                      </tr>
+                    )
+                  )
+                }
+              </tbody>
+            </Table>
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
