a0bc4b91acd4441ee93ee6232269f5f5b49c39d9
Tomasz <wojciktomasz@o2.pl>
2017-09-20 15:20:20 +0200

    Searchbar fully woring with all columns from the react-table

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import users from '../data/users.json'
 import { Grid } from 'semantic-ui-react'
-import ReactTable from "react-table"
-import "react-table/react-table.css"
-
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 
 class Users extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {
+      data: users,
+      search: ''
+    }
+  }
+
   render() {
+    let data = this.state.data
+    let dataToDisplay = data.filter(row => row.email.includes(this.state.search) || row.name.includes(this.state.search) || row.LastName.includes(this.state.search) || row.type.includes(this.state.search))
     return (
       <div>
         <Grid
@@ -16,24 +25,32 @@ class Users extends React.Component {
           verticalAlign='middle'
         >
           <Grid.Column style={{maxWidth: 850}}>
+            <div className='ui fluid icon input' style={{marginTop: '10%'}}>
+              <input
+                placeholder='Search...'
+                value={this.state.search}
+                onChange={e => this.setState({search: e.target.value})}
+              />
+              <i className='search icon'/>
+            </div>
             <ReactTable
-              data={users}
+              data={dataToDisplay}
               columns={[
                 {
-                  Header: "Name",
-                  accessor: "name"
+                  Header: 'Name',
+                  accessor: 'name'
                 },
                 {
-                  Header: "Last name",
-                  accessor: "LastName"
+                  Header: 'Last name',
+                  accessor: 'LastName'
                 },
                 {
-                  Header: "E-mail",
-                  accessor: "email"
+                  Header: 'E-mail',
+                  accessor: 'email'
                 },
                 {
-                  Header: "Type",
-                  accessor: "type"
+                  Header: 'Type',
+                  accessor: 'type'
                 }
               ]}
               sorted={[{
@@ -41,8 +58,9 @@ class Users extends React.Component {
                 desc: false
               }]}
               defaultPageSize={10}
-              className="-striped -highlight"
-              style={{marginTop: '10%'}}/>
+              className='-striped -highlight'
+              style={{marginTop: '0.5%'}}
+            />
           </Grid.Column>
         </Grid>
       </div>
