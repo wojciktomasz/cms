ad1b1b3901117c933a26a19666084ff76d025234
Tomasz Wójcik <jay@desmart.com>
2017-09-22 16:27:24 +0200

    Sending data from the form with a user.type proper value

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Form, Grid, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Segment, Dropdown } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { addUser } from '../reducers/addUser'
 
@@ -19,7 +19,12 @@ class AddUsers extends React.Component {
     }
     this.props.addUser(formData)
   }
+
   render() {
+    const userTypeDropDownOptions = [
+      {value: 'user', text: 'user'},
+      {value: 'admin', text: 'admin'},
+    ]
     return (
       <div>
         <Grid
@@ -31,49 +36,67 @@ class AddUsers extends React.Component {
             <Form onSubmit={this.handleSubmit} className='ui form' size='large'
                   style={{marginTop: '1%'}}>
               <Segment style={{paddingBottom: 0}} stacked>
-                <div className="equal width fields">
-                  <Form.Field>
-                    <input placeholder='Enter name'
-                           ref='name'
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <input placeholder='Enter family name'
-                           ref='lastName'
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <input placeholder='Enter e-mail'
-                           ref='email'
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <input placeholder='Enter password'
-                           ref='password'
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <input placeholder='Enter type'
-                           ref='type'
-                    />
-                  </Form.Field>
+                <div className='equal width fields'>
+                  <div className='required field'>
+                    <label>First name</label>
+                    <Form.Field>
+                      <input placeholder='Enter name'
+                             ref='name'
+                      />
+                    </Form.Field>
+                  </div>
+                  <div className='required field'>
+                    <label>Last name</label>
+                    <Form.Field>
+                      <input placeholder='Enter last name'
+                             ref='lastName'
+                      />
+                    </Form.Field>
+                  </div>
                 </div>
-                <div className="equal width fields">
-                  <Button color='blue' fluid size='medium' type='submit'>Add user</Button>
-                  <Button color='blue' fluid size='medium' type='reset'>Clear Form</Button>
+                <div className='equal width fields'>
+                  <div className='required field'>
+                    <label>E-mail</label>
+                    <Form.Field>
+                      <input placeholder='Enter e-mail'
+                             ref='email'
+                      />
+                    </Form.Field>
+                  </div>
+                  <div className='required field'>
+                    <label>Phone number</label>
+                    <Form.Field>
+                      <input placeholder='Enter phone number'
+                             ref='phone'
+                      />
+                    </Form.Field>
+                  </div>
+                  <div className='field'>
+                    <label>Type</label>
+                    <Form.Field>
+                      <select ref='type' style={{height: '37px'}}>
+                        <option defaultValue='user'>user</option>
+                        <option defaultValue='admin'>admin</option>
+                      </select>
+                    </Form.Field>
+                  </div>
                 </div>
+                  <div className='equal width fields'>
+                    <Button color='blue' fluid size='medium' type='submit'>Add new user</Button>
+                    <Button color='blue' fluid size='medium' type='reset'>Clear Form</Button>
+                  </div>
               </Segment>
             </Form>
           </Grid.Column>
         </Grid>
       </div>
-    )
+  )
   }
-}
+  }
 
-export default connect(
+  export default connect(
   state => ({}),
   dispatch => ({
     addUser: data => dispatch(addUser(data))
   })
-)(AddUsers)
+  )(AddUsers)
